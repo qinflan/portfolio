1b693f8f3711b62d3bae6381040114dd7155574b
qinflan <158785365+qinflan@users.noreply.github.com>
2024-10-27 23:33:23 -0400

    Add Fonts
    Added final fonts for site and created faces for them in global css

--- portfolio/client/src/main.tsx
+++ portfolio/client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './components/hooks/theme/ThemeContext.tsx'
 import App from './App.tsx'
 import './index.css'
+import './clash-display.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
