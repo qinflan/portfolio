b5c8bfb8acb4792cf1bee063cc18912eae156b1a
Quinn Flanigan <qinflanigan@gmail.com>
2024-11-19 14:13:28 -0500

    server.ts config

--- portfolio/server/server.ts
+++ portfolio/server/server.ts
@@ -1,2 +1,30 @@
-import { Express } from "express";
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
+// Express app type
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const PORT: string | number = process.env.PORT || 3001;
+const prefix = "/api";
+
+app.get("/api", (req: Request, res: Response) => {
+  res.json("Hello World!");
+});
+
+(async () => {
+    try {
+      app.listen(PORT, () => {
+        console.log("Server connection successful!")
+      });
+    } catch (err) {
+      console.error("Server unable to start, please try again later")
+      process.exit(1);
+    }
+  })();
+  
+//   app.use(`${prefix}/contactRouter`, contactRouter);
