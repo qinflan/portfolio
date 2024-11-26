1fdbbb6f124ad7269498e875b7e1ac2ea97aa3b6
qinflan <158785365+qinflan@users.noreply.github.com>
2024-11-26 01:42:51 -0500

    vercel attempt

--- portfolio/server/api/index.ts
+++ portfolio/server/api/index.ts
@@ -1,29 +1,8 @@
 import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
 import contactRouter from './contactRoute'
 
-dotenv.config();
+const router = express.Router();
 
-// Express app type
-const app = express();
+router.use("/contact")
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('api/contact', contactRouter);
-export default app;
-
-const PORT: string | number = process.env.PORT || 5000;
-
-(async () => {
-    try {
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-      });
-    } catch (err) {
-      console.error({err});
-      process.exit(1);
-    }
-  })();
+export default router;
--- portfolio/server/index.ts
+++ portfolio/server/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('api/contact', contactRouter);
+app.use('api', contactRouter);
 
 const PORT: string | number = process.env.PORT || 5000;
 
@@ -26,3 +26,5 @@ const PORT: string | number = process.env.PORT || 5000;
       process.exit(1);
     }
   })();
+
+export default app;
