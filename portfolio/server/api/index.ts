3036d81f12e352de159b088323b1493073ee7cad
qinflan <158785365+qinflan@users.noreply.github.com>
2024-11-26 01:19:37 -0500

    vercel config

---
+++ portfolio/server/api/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import contactRouter from './contactRoute'
+
+dotenv.config();
+
+// Express app type
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('api/contact', contactRouter);
+
+const PORT: string | number = process.env.PORT || 5000;
+
+(async () => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+      });
+    } catch (err) {
+      console.error({err});
+      process.exit(1);
+    }
+  })();
