c7f475a8a14e86e54fb252e39ce250ae5c978b76
qinflan <158785365+qinflan@users.noreply.github.com>
2024-11-26 01:37:59 -0500

    vercel config again

--- portfolio/server/api/index.ts
+++ portfolio/server/api/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('api/contact', contactRouter);
+export default app;
 
 const PORT: string | number = process.env.PORT || 5000;
 
