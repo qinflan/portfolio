3593ac547ea6f010b15e6e5e18da3eaa4c175e91
qinflan <158785365+qinflan@users.noreply.github.com>
2024-12-12 03:04:44 -0500

    styles

--- portfolio/client/src/components/ui/RightPanel.tsx
+++ portfolio/client/src/components/ui/RightPanel.tsx
@@ -24,12 +24,11 @@ const RightPanel = () => {
                 in dynamic, fast-paced environments. Above all, I’m committed to creating meaningful digital experiences that make a difference.
                 </p>
             </div>
-            <div className="relative w-full" ref={workRef} id="experience">
-                <Experience />
-            </div>
-
             <div className="relative w-full" ref={projectsRef} id="projects">
                 <Projects/>
+            </div>
+            <div className="relative w-full" ref={workRef} id="experience">
+                <Experience />
             </div>
 
             <div className="relative w-full" ref={designsRef} id="design">
