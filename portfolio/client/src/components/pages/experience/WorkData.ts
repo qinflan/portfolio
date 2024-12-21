9fbdd969129270103ab0e54fcfeea7b5763a71ab
qinflan <158785365+qinflan@users.noreply.github.com>
2024-12-21 18:41:12 -0500

    Update WorkData.ts

--- portfolio/client/src/components/pages/experience/WorkData.ts
+++ portfolio/client/src/components/pages/experience/WorkData.ts
@@ -4,7 +4,7 @@ export const workData = [
       company: "Veolia North America",
       dates: "Jun 2024 - Present",
       description: "Analyzed datasets in Smartsheet to create intuitive, user-friendly dashboards for project tracking. Played a key role in designing a new organizational architecture to streamline Smartsheet integration.",
-      skills: ["Power BI", "Smartsheet", "Jira", "Lucidchart", "Project Management"]
+      skills: ["Python", "Smartsheet", "Jira", "Lucidchart", "IT Project Management"]
     },
     {
       title: "Software Engineer",
@@ -18,7 +18,7 @@ export const workData = [
         company: "Microsoft TEALS",
         dates: "Sep 2024 - Present",
         description: "Supporting computer science education at Boston Latin Academy by assisting with an AP Computer Science course, reviewing student code, and contributing to curriculum development and learning outcomes.",
-        skills: ["Python", "HTML", "CSS"]
+        skills: ["Python", "Code Review", "Data Structures"]
       },
       {
         title: "Fabrication Technician",
